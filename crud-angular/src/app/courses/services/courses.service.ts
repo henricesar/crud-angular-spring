a9582a81c41228f366dec1e7a132325a94a83d79
Henrique Cesar Pellegrino Masetto <henrique.masetto@bradesco.com.br>
2022-03-10 14:40:54 -0300

    Implementando aquivo de service

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Course } from '../model/course';
 import { HttpClient } from '@angular/common/http';
-import { tap } from 'rxjs/operators'
+import { first, tap } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +15,7 @@ export class CoursesService {
   list() {
     return this.httpClient.get<Course[]>(this.API)
     .pipe(
+      first(),
       tap(courses => console.log(courses))
     );
   }
