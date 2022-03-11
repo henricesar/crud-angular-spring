5e732afd2cda79775823ed357a4159a735e425b4
Henrique Cesar Pellegrino Masetto <henrique.masetto@bradesco.com.br>
2022-03-11 17:11:57 -0300

    Configuracao para pegar do back-end

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -1,14 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Course } from '../model/course';
 import { HttpClient } from '@angular/common/http';
-import { delay, first, tap } from 'rxjs/operators'
-
+import { first, tap } from 'rxjs/operators'
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
-  private readonly API = '/assets/courses.json';
+  private readonly API = '/api/courses';
 
   constructor(private httpClient: HttpClient) { }
 
@@ -16,7 +15,6 @@ export class CoursesService {
     return this.httpClient.get<Course[]>(this.API)
     .pipe(
       first(),
-      // delay(5000),
       tap(courses => console.log(courses))
     );
   }
